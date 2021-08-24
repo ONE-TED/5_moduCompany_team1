c780c9be3334811b0d0e85c0ea0f1f43a1c7035d
kwak-bs <iam-bs@naver.com>
2021-08-25 02:00:47 +0900

    [#13] Feat : useTodo 구현 useTodo 커스텀 Hook 함수에서는 Todo State를 전역적으로 관리하고 투두리스트에서 사용되는 함수를 관리합니다.

--- src/Hooks/useTodo.tsx
+++ src/Hooks/useTodo.tsx
@@ -1,3 +1,69 @@
 import { useState, useEffect } from 'react';
+import {Itodo} from 'types';
 
-export {};
+let initialTodos: Itodo[] = [];
+
+export const useTodo = () => {
+  const [todoState, setTodoState] = useState(initialTodos);
+  const [nextIdState, setNextIdState] = useState(0);
+
+  useEffect(() => {
+    loadData();
+  }, []);
+  
+  useEffect(() => {
+    saveData();
+  },[todoState]);
+
+  const loadNextId = (initialTodos : Itodo[]) => {
+    let MaxId = 0;
+    for(let i=0; i<initialTodos.length; i++) {
+      MaxId = Math.max(MaxId, initialTodos[i].id);
+    }
+
+    setNextIdState(MaxId + 1);
+  }
+
+  const increamentNextID = () => {
+    setNextIdState((prevId) => prevId + 1);
+  }
+
+  const removeTodo = (id: number) => {
+    setTodoState((prevState) => 
+      prevState.filter((todo: Itodo) => todo.id !== id)
+    );
+  }
+
+  const createTodo = (todo: Itodo) => {
+    setTodoState((prevState) => prevState.concat({
+      ...todo,
+      id:nextIdState
+      })
+    );
+  }
+
+  const loadData = () => {
+    let data = localStorage.getItem("todos");
+
+    if(data === undefined) data = "";
+    initialTodos = JSON.parse(data!);
+
+    if(initialTodos && initialTodos.length >= 1) {
+      loadNextId(initialTodos);
+    }
+    setTodoState(initialTodos);
+  }
+
+  const saveData = () => {
+    localStorage.setItem("todos", JSON.stringify(todoState));
+  }
+
+  return {
+    todoState,
+    nextIdState,
+    increamentNextID,
+    removeTodo,
+    createTodo
+  }
+}
+
