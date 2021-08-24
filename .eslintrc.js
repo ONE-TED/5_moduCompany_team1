0082058cda692d504a7a05ca0d153c43660edec1
kwak-bs <iam-bs@naver.com>
2021-08-25 00:43:28 +0900

    Chore : 빌드 스크립트 및 eslint 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,13 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
+  env: {
+    browser: true,
+  },
   plugins: ['@typescript-eslint', 'prettier'],
   extends: [
-    'airbnb',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:prettier/recommended',
+    'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
   ],
   rules: {
     'linebreak-style': 0,
@@ -16,13 +16,10 @@ module.exports = {
     'import/extensions': 0,
     'no-use-before-define': 0,
     'import/no-unresolved': 0,
-    'import/no-extraneous-dependencies': 0, // 테스트 또는 개발환경을 구성하는 파일에서는 devDependency 사용을 허용
-    'no-shadow': 0,
     'react/prop-types': 0,
     'react/jsx-filename-extension': [
       2,
       { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
     ],
-    'jsx-a11y/no-noninteractive-element-interactions': 0,
   },
 };
