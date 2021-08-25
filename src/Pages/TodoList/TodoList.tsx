b3633ffc6c6c6d8f5064632d3885ecc40147973a
yujin mac <kimyu28jin@gmail.com>
2021-08-25 14:34:59 +0900

    [#20] Chore : 공통 데이터, 파일 수정
    - 데이터 추가 및 공통영역 수정

--- src/Pages/TodoList/TodoList.tsx
+++ src/Pages/TodoList/TodoList.tsx
@@ -1,47 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
-
 import Header from 'Components/Header';
+import data from 'data.json';
+import { Itodo } from 'types';
 
-const TodoList: React.FC = () => {
+const TodoList = () => {
+  const todoData = data as Itodo[];
+  const [list, setList] = useState<Itodo[] | null>(null);
+
   return (
     <TodoListTemplate>
       <Header />
-
       <TodoItemsLayout>
         <ul style={{ padding: '20px' }}>
-          <Li>
-            <span style={{ fontSize: '20px' }}>코드리뷰하기</span>
-            <div style={{ display: 'flex', alignItems: 'center' }}>
-              <button>icon</button>
-              <div>selectBox</div>
-              <button>휴지통</button>
-            </div>
-          </Li>
-          <Li>
-            <span style={{ fontSize: '20px' }}>컴포넌트설계2</span>
-            <div style={{ display: 'flex' }}>
-              <button>icon</button>
-              <div>selectBox</div>
-              <button>휴지통</button>
-            </div>
-          </Li>
-          <Li>
-            <span style={{ fontSize: '20px' }}>밥먹기3</span>
-            <div style={{ display: 'flex' }}>
-              <button>icon</button>
-              <div>selectBox</div>
-              <button>휴지통</button>
-            </div>
-          </Li>
-          <Li>
-            <span style={{ fontSize: '20px' }}>코딩하기4</span>
-            <div style={{ display: 'flex' }}>
-              <button>icon</button>
-              <div>selectBox</div>
-              <button>휴지통</button>
-            </div>
-          </Li>
+          {todoData.map((item: Itodo) => (
+            <Li key={item.id}>
+              <span style={{ fontSize: '20px' }}>{item.taskName}</span>
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                <button>icon</button>
+                <div>selectBox</div>
+                <button>휴지통</button>
+              </div>
+            </Li>
+          ))}
         </ul>
       </TodoItemsLayout>
     </TodoListTemplate>
