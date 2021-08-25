6566c44c3b8eb7dea43fb407e05494a73bbcd7ea
kwak-bs <iam-bs@naver.com>
2021-08-25 16:37:07 +0900

    [#13] Feat : TodoList Item 목록 보이기

--- src/Pages/TodoList/TodoList.tsx
+++ src/Pages/TodoList/TodoList.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Header from 'Components/Header';
 import data from 'data.json';
-import TodoCreate from 'Components/Todo/Todocreate';
+import TodoCreate from 'Components/Todo/Create';
 import { useTodo } from 'Hooks/useTodo';
 import { style } from './TodoListStyle';
 import { Itodo } from 'types';
@@ -10,7 +10,7 @@ const TodoList: React.FC = () => {
   const { todoState, nextIdState, incrementNextId, removeTodo, createTodo } =
     useTodo();
 
-  const todoData = data as Itodo[];
+  //const todoData = data as Itodo[];
   const [list, setList] = useState<Itodo[] | null>(null);
 
   return (
@@ -24,7 +24,7 @@ const TodoList: React.FC = () => {
         />
         <TodoItemsLayout>
           <ul style={{ padding: '20px' }}>
-            {todoData.map((item: Itodo) => (
+            {todoState.map((item: Itodo) => (
               <Li key={item.id}>
                 <span style={{ fontSize: '20px' }}>{item.taskName}</span>
                 <div style={{ display: 'flex', alignItems: 'center' }}>
