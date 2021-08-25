a82e561c4378785c0d726dd524fc8a3867538e6e
kwak-bs <iam-bs@naver.com>
2021-08-26 04:43:07 +0900

    [#13] Style : 아이템 레이아웃 DueDate UI 추가 (ex. D-2)

--- src/Components/Todo/Item/TodoItem.tsx
+++ src/Components/Todo/Item/TodoItem.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Itodo } from 'types';
 import { style } from './TodoItemStyle';
 
@@ -9,14 +9,33 @@ interface TodoItemProps {
 
 const TodoItem = ({ removeTodo, todo }: TodoItemProps) => {
   const [done, setDone] = useState(todo.status);
+  const [dDay, setDday] = useState<number>(0);
+
+  useEffect(() => {
+    calculateDday(todo.updatedAt);
+  }, []);
 
   const handleRemove = () => {
     removeTodo(todo.id);
   };
 
+  const calculateDday = (dateString: string) => {
+    const dateStringArry: string[] = dateString.split(' ')[0].split('-');
+    console.log(dateStringArry);
+    const dDay: Date = new Date(
+      parseInt(dateStringArry[0]),
+      parseInt(dateStringArry[1]) - 1,
+      parseInt(dateStringArry[2]),
+    );
+    const today: Date = new Date();
+    const gap = dDay.getTime() - today.getTime();
+    const result = Math.ceil(gap / (1000 * 60 * 60 * 24));
+    setDday(result);
+  };
   return (
     <TodoItemBlock>
       <Text>{todo.taskName}</Text>
+      <Text>{dDay > 0 ? `D -${dDay}` : `D +${-dDay}`}</Text>
       <ElementBlock>
         <button style={{ cursor: 'pointer', marginRight: '10px' }}>수정</button>
         <div style={{ cursor: 'pointer', marginRight: '10px' }}>selectBox</div>
