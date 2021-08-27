c27fdd984ebcfc9553f8ddfbd4410686ff253004
kwak-bs <iam-bs@naver.com>
2021-08-27 17:46:26 +0900

    [#45] Feat : Focus Todo 기능 추가

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
 import { TodoDate } from 'utils/todoDate';
 import { ReactComponent as filterIcon } from 'Assets/icons/filter.svg';
 
 const Header: React.FC = () => {
+  const [focusTodo, setFocusTodo] = useState<boolean>(false);
+  const [focusValue, setFocusValue] = useState<string>('');
+
+  useEffect(() => {
+    loadFocusTodo();
+  }, [focusTodo]);
+
+  const loadFocusTodo = () => {
+    const focusTodo = localStorage.getItem('focus');
+
+    if (focusTodo === null) {
+      setFocusTodo(false);
+    } else {
+      setFocusTodo(true);
+    }
+  };
+
   const getTodayDate = () => {
     const today = new TodoDate();
     return today.getToday();
@@ -14,13 +31,76 @@ const Header: React.FC = () => {
     //sidedrawer 토글 로직
   };
 
+  const handleFocusTodo = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFocusValue(e.target.value);
+  };
+
+  const handleSubmitFocus = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    localStorage.setItem('focus', focusValue);
+    setFocusTodo(true);
+  };
+
+  const handleModifyFocusTodo = () => {
+    localStorage.removeItem('focus');
+    setFocusValue('');
+    setFocusTodo(false);
+  };
+
   return (
     <>
       <HeaderLayout>
-        <HeaderTitleLayout>{`Today is : ${getTodayDate()}`}</HeaderTitleLayout>
-        <FilterIconLayout>
-          <FilterIcon onClick={handleOnClick} />
-        </FilterIconLayout>
+        <div className="time" style={{ display: 'flex' }}>
+          <HeaderTitleLayout>{`Today is : ${getTodayDate()}`}</HeaderTitleLayout>
+          <FilterIconLayout>
+            <FilterIcon onClick={handleOnClick} />
+          </FilterIconLayout>
+        </div>
+        <div>
+          {focusTodo ? (
+            <>
+              <div className="focusValueWrap" style={{ display: 'flex' }}>
+                <h3>{focusValue}</h3>
+                <button
+                  style={{
+                    background: 'black',
+                    color: 'white',
+                    cursor: 'pointer',
+                    width: '20%',
+                  }}
+                  onClick={handleModifyFocusTodo}
+                >
+                  수정
+                </button>
+              </div>
+            </>
+          ) : (
+            <>
+              <form className="inputForm" onSubmit={handleSubmitFocus}>
+                <h2>오늘 가장 집중해야할 일을 적어보세요.</h2>
+                <div className="inputWrap" style={{ display: 'flex' }}>
+                  <input
+                    placeholder="오늘 가장 집중이 필요한 한가지 일을 작성해보세요"
+                    value={focusValue}
+                    onChange={handleFocusTodo}
+                    style={{ width: '100%' }}
+                  />
+                  <button
+                    style={{
+                      background: 'black',
+                      color: 'white',
+                      cursor: 'pointer',
+                      width: '20%',
+                    }}
+                  >
+                    등록
+                  </button>
+                </div>
+              </form>
+            </>
+          )}
+        </div>
       </HeaderLayout>
     </>
   );
@@ -28,7 +108,7 @@ const Header: React.FC = () => {
 
 const HeaderLayout = styled.div`
   position: sticky;
-  display: flex;
+  display: inline-block;
   justify-content: center;
 
   padding: 1rem;
